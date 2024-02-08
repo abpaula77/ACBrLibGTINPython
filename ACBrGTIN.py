86699f9d61cdd5268a6ddc4c3f83763689933c3d
Alexandre Ballestero de Paula <82414677+abpaula77@users.noreply.github.com>
2024-02-08 20:37:09 -0300

    implementação da consulta

--- ACBrGTIN.py
+++ ACBrGTIN.py
@@ -1,64 +1,54 @@
 import ctypes
+import os
 
-while True:
-    print("Menu:")
-    print("1. cwsBrasilAPI")
-    print("2. cwsReceitaWS")
-    print("Digite 0 para sair")
-
-    try:
-        provedor = int(input("Escolha uma opção: "))
-
-        if provedor == 1:
-            print("Você escolheu a Opção 1.")
-        elif provedor == 2:
-            print("Você escolheu a Opção 2.")
+def obter_GTIN():
+    while True:
+        entrada = input("Digite o codigo de barras(GTIN): ")
+        
+        # Verifica se a entrada possui apenas dígitos e tem 13 caracteres
+        if entrada.isdigit() and len(entrada) == 13:
+            return entrada
         else:
-            print("Encerrando o programa.")
-            break
-
-        # Carregar a DLL
-        acbr_lib = ctypes.CDLL(r'C:\ACBrLibConsultaCNPJPython\ACBrConsultaCNPJ64.dll')
-        inicializa = acbr_lib.CNPJ_Inicializar(r'C:\ACBrLibConsultaCNPJPython\ACBrConsultaCNPJ.INI'.encode("utf-8"),"".encode("utf-8"))
-
-        # Definir a assinatura da função
-        acbr_lib.CNPJ_Consultar.argtypes = (ctypes.c_char_p, ctypes.c_int, ctypes.c_char_p, ctypes.POINTER(ctypes.c_int))
-        acbr_lib.CNPJ_Consultar.restype = ctypes.c_int
-
-        # Definir variáveis para armazenar os resultados
-        #sResposta = ctypes.create_string_buffer(256)  # Substitua 256 pelo tamanho máximo esperado da string
-        esTamanho = ctypes.c_int(1024)
-        # Definir o valor do CNPJ como uma string
-        # cnpj_valor = "27329866000105"
-        cnpj_valor =  input("Digite o CNPJ (somente numeros): ")
-
-        # Criar um buffer de string com espaço suficiente
-        sCNPJ = ctypes.create_string_buffer(15)  # +1 para o caractere nulo de terminação
-
-        # Copiar o valor do CNPJ para o buffer
-        ctypes.memmove(sCNPJ, cnpj_valor.encode('utf-8'), len(cnpj_valor))
-
-        # Chamar a função CNPJ_Versao
-        sResposta = ctypes.create_string_buffer(1024) 
-        resultado = acbr_lib.CNPJ_Consultar(sCNPJ,provedor,sResposta,ctypes.byref(esTamanho))
-
-        #sVersao = ctypes.create_string_buffer(15) 
-        #resultado = acbr_lib.CNPJ_Versao(sVersao, ctypes.byref(esTamanho))
-
-        # Verificar o resultado
-        if resultado == 0:
-            print(f"tamanho da resposta: {esTamanho}")
-            sMensagem = ctypes.create_string_buffer(534 + 1)    
-            acbr_lib.CNPJ_UltimoRetorno(sMensagem,ctypes.byref(esTamanho))
-            print(sMensagem.value.decode('utf-8'))
-        else:
-            print(f"Falha ao consultar CNPJ. Código de erro: {resultado}")
-
-        acbr_lib.CNPJ_Finalizar()
+            print("Entrada inválida. Certifique-se de digitar apenas números e ter 13 caracteres.")
 
 
-    except ValueError:
-        print("Entrada inválida. Por favor, digite um número.")
+os.system('cls')
+#codigo = '7894900709841'
+codigo = obter_GTIN()
+
+# Carregar a DLL
+acbr_lib = ctypes.CDLL(r'C:\ACBrLibGTINPython\ACBrGTIN64.dll')
+inicializa = acbr_lib.GTIN_Inicializar(r'C:\ACBrLibGTINPython\ACBrGTIN.INI'.encode("utf-8"),"".encode("utf-8"))
+
+# Definir a assinatura da função
+acbr_lib.GTIN_Consultar.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.POINTER(ctypes.c_int))
+acbr_lib.GTIN_Consultar.restype = ctypes.c_int
+
+# Definir variáveis para armazenar os resultados
+sResposta = ctypes.create_string_buffer(1024) 
+esTamanho = ctypes.c_int(1024)
+
+# Criar um buffer de string com espaço suficiente
+sGTIN = ctypes.create_string_buffer(14)  # +1 para o caractere nulo de terminação
+
+# Copiar o valor do GTIN para o buffer
+ctypes.memmove(sGTIN, codigo.encode('utf-8'), len(codigo))
+
+# Chamar a função GTIN_Consultar
+resultado = acbr_lib.GTIN_Consultar(sGTIN,sResposta,ctypes.byref(esTamanho))
+
+# Verificar o resultado
+if resultado == 0:
+    print(f"tamanho da resposta: {esTamanho}")
+    sMensagem = ctypes.create_string_buffer(534 + 1)    
+    acbr_lib.GTIN_UltimoRetorno(sMensagem,ctypes.byref(esTamanho))
+    print(sMensagem.value.decode('utf-8'))
+else:
+    print(f"Falha ao consultar GTIN. Código de erro: {resultado}")
+
+acbr_lib.GTIN_Finalizar()
+
+
 
 
 
